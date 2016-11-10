006f36528427cb08728cc32d4ba1fbb1cf755744
VidyaBhalodia <vidya.bhalodia@gmail.com>
2016-11-10 06:03:51 -0500

    database seeeder

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,71 @@
 |
 */
 
+Route::get('/debug', function() {
+
+    echo '<pre>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(config('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    /*
+    The following line will output your MySQL credentials.
+    Uncomment it only if you're having a hard time connecting to the database and you
+    need to confirm your credentials.
+    When you're done debugging, comment it back out so you don't accidentally leave it
+    running on your live server, making your credentials public.
+    */
+    //print_r(config('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    }
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    }
+
+    echo '</pre>';
+
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/books', function () {
+    return 'here are all the books';
+});
+
+Route::get('/books/create', function() {
+    $view  = '<form method="POST" action="/books/create">';
+    $view .= csrf_field(); # This will be explained more later
+    $view .= '<label>Title: <input type="text" name="title"></label>';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+    return $view;
+});
+
+Route::post('/books/create', function() {
+    dd(Request::all());
+});
+
+Route::get('/books/show/{title}', function($title) {
+    return 'Results for the book: '.$title;
+});
+
+Route::get('/books/show/{title?}', function($title = '') {
+
+    if($title == '') {
+        return 'Your request did not include a title.';
+    }
+
+    return 'Results for the book: '.$title;
 });
